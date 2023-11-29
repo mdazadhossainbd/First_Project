3f5c9f5c075b5bc3bec80eb1aebedbd74fdb48cb
mdazadhossainbd <150763003+mdazadhossainbd@users.noreply.github.com>
2023-11-29 08:44:15 +0600

    first commit

---
+++ lecture2task1.js
@@ -0,0 +1,18 @@
+
+let firstNum = Number(prompt('Enter first number: '))
+let secondNum = Number(prompt('Enter second number: '))
+
+let sum = firstNum + secondNum
+document.write("Sum: " + sum)
+
+let sub = firstNum - secondNum
+document.write("<br> Substruction: " + sub)
+
+let multi = firstNum * secondNum
+document.write("<br> Multiplication: " + multi)
+
+let div = firstNum / secondNum
+document.write("<br> Division: " + div)
+
+let modu = firstNum % secondNum
+document.write("<br> Modulus:"+modu)
---
+++ lecture2task2.js
@@ -0,0 +1,32 @@
+// area of circle
+
+let pi = 3.1416
+let r = Number(prompt('Enter R value: '))
+
+let areaOfCircle = pi * (r**2)
+document.write('Area of circle: ' + areaOfCircle)
+
+// area of square
+
+let a = Number(prompt('Enter value of A: '))
+
+areaOfSquare = a**2
+document.write('area of square: ' + areaOfSquare)
+
+let height1 = Number(prompt('Enter the length: '))
+let width = Number(prompt('Enter the width: '))
+
+// area of ractangle
+
+let areaOfRectangle = width * height1
+document.write('Area of ractangle: ' + areaOfRectangle)
+
+
+// area of triangle
+
+
+let base = Number(prompt('Enter the base: '))
+let height = Number(prompt('Enter the height: '))
+
+let areaOfTriangle = 0.5 * base * height
+document.write('Area of triangle: ' + areaOfTriangle)
